6a1a965f4f5df9b96f9ae7b3f2b98f28b9a59b11
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-03-30 20:47:21 -0300

    Following the Style Sheet and commenting the methods

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Potion.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Potion.java
@@ -3,14 +3,17 @@ package trl.entity.item;
 import trl.main.Game;
 import trl.map.Map;
 
-public class Potion extends Item {
-    public Potion(Map map) {
-        super(map);
-        init();
-    }
-	
-    public void init() {
-        this.image = Game.getImageManager().potion;
-        this.loc = map.placeEntity(this, map.getRandomNodeInRoom());
-    }    
+public class Potion extends Item{
+
+	public Potion(Map map){
+
+		super ( map );
+		init ();
+	}
+	// Initializes Potion
+	public void init(){
+
+		this.image = Game.getImageManager ().potion;
+		this.loc = map.placeEntity ( this , map.getRandomNodeInRoom () );
+	}
 }
