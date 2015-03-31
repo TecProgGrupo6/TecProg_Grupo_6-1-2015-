2e44e9314c058bce2d6a721cd5e5b14a71ce16e3
MaxOliveira <maxwell_oliveira2@hotmail.com>
2015-03-30 23:32:20 -0300

    Aplying the StyleSheet in the class Toolbar

--- Library/src/Toolbar.java
+++ Library/src/Toolbar.java
@@ -4,16 +4,16 @@
 import javax.swing.*;
 
 public class Toolbar extends JToolBar {
+	
 	/***************************************************************************
 	 ***      declaration of the private variables used in the program       ***
 	 ***************************************************************************/
 
 	//for creating the buttons to use them in ToolBar
 	public JButton[] button;
+	
 	//for creating the name of the image file 24*24
 	public String[] imageName24 = {"images/Add24.gif", "images/List24.gif",
-	                           
-	                             
 	                           "images/Add24.gif",
 	                               "images/List24.gif", 
 	                            
@@ -21,20 +21,32 @@ public class Toolbar extends JToolBar {
 	                               "images/Import24.gif",
 	                          
 	                               "images/Exit24.gif"};
+	
 	//for creating the tipText for the toolbar
 	public String[] tipText = {"Add Books", "List All Books",
-	                           
 	                           "Add Members", "List Members", 
 	                           "Search", "Borrow Books", "Return Books", "Exit"};
 
+	//Creating a toolbar
 	public Toolbar() {
+		
 		button = new JButton[19];
-		for (int i = 0; i < imageName24.length; i++) {
-			if (i == 2|| i ==4 || i == 5|| i == 7)
+		
+		for ( int i = 0; i < imageName24.length; i++ ) {
+			
+			if ( i == 2|| i ==4 || i == 5|| i == 7 ) {
+				
 			//for adding separator to the toolBar
 				addSeparator();
+			
+			}
+			else {
+				//No action
+			}
+			
 			//for adding the buttons to toolBar
 			add(button[i] = new JButton(new ImageIcon(ClassLoader.getSystemResource(imageName24[i]))));
+			
 			//for setting the ToolTipText to the button
 			button[i].setToolTipText(tipText[i]);
 		}
