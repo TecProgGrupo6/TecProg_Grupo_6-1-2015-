da2d8eae7b08d475f88751b0b717208bb78507ab
MaxOliveira <maxwell_oliveira2@hotmail.com>
2015-03-30 23:26:12 -0300

    Aplying the StyleSheet in the class StatusBar

--- Library/src/StatusBar.java
+++ Library/src/StatusBar.java
@@ -11,15 +11,19 @@ import java.awt.*;
  */
 
 public class StatusBar extends JPanel {
+	
 	/***************************************************************************
 	 ***      declaration of the private variables used in the program       ***
 	 ***************************************************************************/
+	
 	private JLabel statusBar = new JLabel("  ");
 
 	//constructor of StatusBar
 	public StatusBar() {
-		statusBar.setFont(new Font("Tahoma", Font.BOLD, 9));
-		this.add(statusBar);
-		this.setBorder(new SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+		
+		statusBar.setFont(new Font( "Tahoma", Font.BOLD, 9) );
+		this.add( statusBar );
+		this.setBorder( new SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED) );
+		
 	}
 }
