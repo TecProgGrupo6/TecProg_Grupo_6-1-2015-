a3fa4b5f949570aa3bc5a775b9600ac410d742f4
MaxOliveira <maxwell_oliveira2@hotmail.com>
2015-04-13 20:53:37 -0300

    Following the StyleSheet and commenting the methods

--- Library/src/Borrow.java
+++ Library/src/Borrow.java
@@ -13,10 +13,19 @@ public class Borrow {
 	private Statement statement = null;
 	private ResultSet resultSet = null;
 
+	//Inform the book ID
 	private int bookID;
+	
+	//Represent the member ID
 	private int memberID;
+	
+	//Represent the day of the borrowing
 	private Date dayOfBorrowed;
+	
+	//Represent the day of the return
 	private Date dayOfReturn;
+	
+	//URL connection
 	private String URL = "jdbc:odbc:JLibrary";
 
 	public Borrow() {
@@ -38,63 +47,75 @@ public class Borrow {
 		return dayOfReturn;
 	}
 
+	//Establishing the connection
 	public void connection() {
 		try {
 			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 		}
-		catch (ClassNotFoundException cnfe) {
+		catch ( ClassNotFoundException cnfe ) {
 			System.out.println("Borrow.java\n" + cnfe.toString());
 		}
-		catch (Exception e) {
+		catch ( Exception e ) {
 			System.out.println("Borrow.java\n" + e.toString());
 		}
+		
 		/***************************************************************
 		 * for making the connection,creating the statement and update *
 		 * the table in the database. After that,closing the statmenet *
 		 * and connection. There is catch block SQLException for error *
 		 ***************************************************************/
 		try {
+			
 			connection = DriverManager.getConnection(URL);
 			statement = connection.createStatement();
 			resultSet = statement.executeQuery("SELECT * FROM Borrow");
-			while (resultSet.next()) {
+			
+			while ( resultSet.next() ) {
+				
 				bookID = resultSet.getInt(1);
 				memberID = resultSet.getInt(2);
 				dayOfBorrowed = resultSet.getDate(3);
 				dayOfReturn = resultSet.getDate(4);
+				
 			}
+			
 			resultSet.close();
 			statement.close();
 			connection.close();
+			
 		}
-		catch (SQLException SQLe) {
+		catch ( SQLException SQLe ) {
 			System.out.println("Borrow.java\n" + SQLe.toString());
 		}
 	}
 
-	public void update(String Query) {
+	//Updating the borrowing
+	public void update( String Query ) {
 		try {
 			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 		}
-		catch (ClassNotFoundException cnfe) {
+		catch ( ClassNotFoundException cnfe ) {
 			System.out.println("Borrow.java\n" + cnfe.toString());
 		}
-		catch (Exception e) {
+		catch ( Exception e ) {
 			System.out.println("Borrow.java\n" + e.toString());
 		}
+		
 		/***************************************************************
 		 * for making the connection,creating the statement and update *
 		 * the table in the database. After that,closing the statmenet *
 		 * and connection. There is catch block SQLException for error *
 		 ***************************************************************/
 		try {
+			
 			connection = DriverManager.getConnection(URL);
 			statement = connection.createStatement();
 			statement.executeUpdate(Query);
 			statement.close();
 			connection.close();
+			
 		}
-		catch (SQLException SQLe) {
+		catch ( SQLException SQLe ) {
 			System.out.println("Borrow.java\n" + SQLe.toString());
 		}
 	}
