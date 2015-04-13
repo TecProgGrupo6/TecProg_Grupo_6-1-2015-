5ef585dbb030b299b860c4dcc78510841c3c72b0
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-04-13 18:35:07 -0300

    adding comments

--- Library/src/ListSearchBooks.java
+++ Library/src/ListSearchBooks.java
@@ -13,16 +13,17 @@ import java.sql.SQLException;
  * A public class
  */
 public class ListSearchBooks extends JInternalFrame{
+
 	/***************************************************************************
 	 *** declaration of the private variables used in the program ***
 	 ***************************************************************************/
 
 	// For creating the North Panel.
-	private JPanel northPanel = new JPanel();
+	private JPanel northPanel = new JPanel ();
 	// For creating the Center Panel.
-	private JPanel centerPanel = new JPanel();
+	private JPanel centerPanel = new JPanel ();
 	// For creating the label.
-	private JLabel label = new JLabel("THE LIST FOR THE SEARCHED BOOKS");
+	private JLabel label = new JLabel ( "THE LIST FOR THE SEARCHED BOOKS" );
 	// For creating the button.
 	private JButton printButton;
 	// For creating the table.
@@ -36,13 +37,15 @@ public class ListSearchBooks extends JInternalFrame{
 	private ResultSetTableModel tableModel;
 
 	// Constructor of listSearchBooks.
-	public ListSearchBooks(String query){
+	public ListSearchBooks ( String query ){
+
 		// For setting the title for the internal frame.
-		super("Searched Books", false, true, false, true);
+		super ( "Searched Books" , false , true , false , true );
 		// For setting the icon.
-		setFrameIcon(new ImageIcon(ClassLoader.getSystemResource("images/List16.gif")));
+		setFrameIcon ( new ImageIcon (
+				ClassLoader.getSystemResource ( "images/List16.gif" ) ) );
 		// For getting the graphical user interface components display area.
-		Container cp = getContentPane();
+		Container cp = getContentPane ();
 
 		/***********************************************************************
 		 * for setting the required information for the ResultSetTableModel
@@ -55,126 +58,188 @@ public class ListSearchBooks extends JInternalFrame{
 		// For bassing the required information to the ResultSetTableModel.
 		// Object.
 		try{
-			tableModel = new ResultSetTableModel(JDBC_DRIVER, DATABASE_URL, DEFAULT_QUERY);
+			tableModel = new ResultSetTableModel ( JDBC_DRIVER , DATABASE_URL ,
+					DEFAULT_QUERY );
 			// For setting the Query.
 			try{
-				tableModel.setQuery(DEFAULT_QUERY);
-			}catch (SQLException sqlException){
-				
+				tableModel.setQuery ( DEFAULT_QUERY );
+			}catch ( SQLException sqlException ){
+
+				// Do nothing
+
 			}
-		}catch (ClassNotFoundException classNotFound){
-			
-		}catch (SQLException sqlException){
-			
+		}catch ( ClassNotFoundException classNotFound ){
+
+			// Do nothing
+
+		}catch ( SQLException sqlException ){
+
+			// Do nothing
+
 		}
 
 		// For setting the table with the information.
-		table = new JTable(tableModel);
+		table = new JTable ( tableModel );
 		// For setting the size for the table.
-		table.setPreferredScrollableViewportSize(new Dimension(990, 200));
+		table.setPreferredScrollableViewportSize ( new Dimension ( 990 , 200 ) );
 		// For setting the font.
-		table.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		table.setFont ( new Font ( "Tahoma" , Font.PLAIN , 12 ) );
 		// For setting the scrollpane to the table.
-		scrollPane = new JScrollPane(table);
+		scrollPane = new JScrollPane ( table );
 
 		// For setting the size for the table columns.
-		for ( int i = 0 ; i < 13 ; i++ ){
-			column = table.getColumnModel().getColumn(i);
-			if ( i == 0 ){ // BookID.
-				column.setPreferredWidth(20);
+		for ( int i = 0 ; i< 13 ; i++ ){
+			column = table.getColumnModel ().getColumn ( i );
+			if ( i== 0 ){ // BookID.
+				column.setPreferredWidth ( 20 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 1 ){ // Subject.
-				column.setPreferredWidth(100);
+			
+			if ( i== 1 ){ // Subject.
+				column.setPreferredWidth ( 100 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 2 ){ // Title.
-				column.setPreferredWidth(150);
+			
+			if ( i== 2 ){ // Title.
+				column.setPreferredWidth ( 150 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 3 ){ // Auther.
-				column.setPreferredWidth(50);
+			
+			if ( i== 3 ){ // Auther.
+				column.setPreferredWidth ( 50 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 4 ){ // Publisher.
-				column.setPreferredWidth(70);
+			
+			if ( i== 4 ){ // Publisher.
+				column.setPreferredWidth ( 70 );
 			}
-			if ( i == 5 ){ // Copyright.
-				column.setPreferredWidth(40);
+			if ( i== 5 ){ // Copyright.
+				column.setPreferredWidth ( 40 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 6 ){ // Edition.
-				column.setPreferredWidth(40);
+			
+			if ( i== 6 ){ // Edition.
+				column.setPreferredWidth ( 40 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 7 ){ // Pages.
-				column.setPreferredWidth(40);
+			
+			if ( i== 7 ){ // Pages.
+				column.setPreferredWidth ( 40 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 8 ){ // NumberOfBooks.
-				column.setPreferredWidth(80);
+			
+			if ( i== 8 ){ // NumberOfBooks.
+				column.setPreferredWidth ( 80 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 9 ){ // ISBN.
-				column.setPreferredWidth(70);
+			
+			if ( i== 9 ){ // ISBN.
+				column.setPreferredWidth ( 70 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 10 ){ // Library.
-				column.setPreferredWidth(30);
+			
+			if ( i== 10 ){ // Library.
+				column.setPreferredWidth ( 30 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 11 ){ // Availble.
-				column.setPreferredWidth(30);
+			
+			if ( i== 11 ){ // Availble.
+				column.setPreferredWidth ( 30 );
+			}else{
+				
+				// Do nothing
 			}
-			if ( i == 12 ){ // ShelfNo.
-				column.setPreferredWidth(30);
+			
+			if ( i== 12 ){ // ShelfNo.
+				column.setPreferredWidth ( 30 );
+			}else{
+				
+				// Do nothing
 			}
+			
 		}
 		// For setting the font to the label.
-		label.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label.setFont ( new Font ( "Tahoma" , Font.BOLD , 14 ) );
 		// For setting the layout to the panel.
-		northPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		northPanel.setLayout ( new FlowLayout ( FlowLayout.CENTER ) );
 		// For adding the label to the panel.
-		northPanel.add(label);
+		northPanel.add ( label );
 		// For adding the panel to the container.
-		cp.add("North", northPanel);
+		cp.add ( "North" , northPanel );
 
 		// For setting the layout to the panel.
-		centerPanel.setLayout(new BorderLayout());
+		centerPanel.setLayout ( new BorderLayout () );
 		// For creating an image for the button.
-		ImageIcon printIcon = new ImageIcon(ClassLoader.getSystemResource("images/Print16.gif"));
+		ImageIcon printIcon = new ImageIcon ( ClassLoader.getSystemResource ( "images/Print16.gif" ) );
 		// For adding the button to the panel.
-		printButton = new JButton("print the books", printIcon);
+		printButton = new JButton ( "print the books" , printIcon );
 		// For setting the tip text.
-		printButton.setToolTipText("Print");
+		printButton.setToolTipText ( "Print" );
 		// For setting the font to the button.
-		printButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		printButton.setFont ( new Font ( "Tahoma" , Font.PLAIN , 12 ) );
 		// For adding the button to the panel.
-		centerPanel.add(printButton, BorderLayout.NORTH);
+		centerPanel.add ( printButton , BorderLayout.NORTH );
 		// For adding the scrollpane to the panel.
-		centerPanel.add(scrollPane, BorderLayout.CENTER);
+		centerPanel.add ( scrollPane , BorderLayout.CENTER );
 		// For setting the border to the panel.
-		centerPanel.setBorder(BorderFactory.createTitledBorder("Books:"));
+		centerPanel.setBorder ( BorderFactory.createTitledBorder ( "Books:" ) );
 		// For adding the panel to the container.
-		cp.add("Center", centerPanel);
+		cp.add ( "Center" , centerPanel );
 
 		// For adding the actionListener to the button.
-		printButton.addActionListener(new ActionListener(){
+		printButton.addActionListener ( new ActionListener (){
+
 			public void actionPerformed( ActionEvent ae ){
-				Thread runner = new Thread(){
+
+				Thread runner = new Thread (){
+
 					public void run(){
+
 						try{
-							PrinterJob prnJob = PrinterJob.getPrinterJob();
-							prnJob.setPrintable(new PrintingBooks(DEFAULT_QUERY));
-							if ( !prnJob.printDialog() ){
+							
+							PrinterJob prnJob = PrinterJob.getPrinterJob ();
+							prnJob.setPrintable ( new PrintingBooks ( DEFAULT_QUERY ) );
+							if ( !prnJob.printDialog () ){
+								
 								return;
 							}else{
-								
+
+								// Do nothing
 							}
-							setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-							prnJob.print();
-							setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-						}catch (PrinterException ex){
-							System.out.println("Printing error: " + ex.toString());
+							setCursor ( Cursor.getPredefinedCursor ( Cursor.WAIT_CURSOR ) );
+							prnJob.print ();
+							setCursor ( Cursor.getPredefinedCursor ( Cursor.DEFAULT_CURSOR ) );
+						}catch ( PrinterException ex ){
+							System.out.println ( "Printing error: "+ ex.toString () );
 						}
 					}
 				};
-				runner.start();
+				runner.start ();
 			}
-		});
+		} );
 		// For setting the visible to true.
-		setVisible(true);
+		setVisible ( true );
 		// To show the frame.
-		pack();
+		pack ();
 	}
 }
