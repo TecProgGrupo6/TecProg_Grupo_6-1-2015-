95a7a8ce5417ebacd0e169ee9497db58abfec97c
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-04-26 20:04:07 -0300

    Simplifying the class Actor

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/actor/Actor.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/actor/Actor.java
@@ -9,6 +9,7 @@ import java.util.Random;
 
 import trl.entity.Entity;
 import trl.entity.enemy.Enemy;
+import trl.entity.player.Player;
 import trl.gamestate.GameplayState;
 import trl.main.Game;
 import trl.map.Map;
@@ -19,18 +20,18 @@ import trl.sound.SoundManager;
 public abstract class Actor extends Entity{
 
 	// Life of the Player
-	protected int hp ;
-	
+	protected int hp;
+
 	// Represent maximum amount of hp
 	protected int maxHP;
-	
+
 	// Represent the attack force
 	protected int attack;
 
 	// Quantity of damage caused in player
 	protected int damageTaken;
 
-	// Quantity of damage caused in other 
+	// Quantity of damage caused in other
 	protected int damageDealt;
 
 	// Sound Manager
@@ -40,7 +41,7 @@ public abstract class Actor extends Entity{
 	protected boolean acted;
 
 	// Describe if the player was attacked or not
-	protected boolean attacked; 
+	protected boolean attacked;
 
 	// Describe if the player was moved or not
 	protected boolean moved; // Did I move this tick?
@@ -69,7 +70,7 @@ public abstract class Actor extends Entity{
 	// Turns on node
 	protected int turnsOnNode;
 
-	public Actor(Map map){
+	public Actor ( Map map ){
 
 		super ( map );
 		initActor ();
@@ -95,7 +96,7 @@ public abstract class Actor extends Entity{
 
 	public boolean isAlive(){
 
-		return hp> 0;
+		return hp > 0;
 	}
 
 	public int getDamageTaken(){
@@ -112,9 +113,11 @@ public abstract class Actor extends Entity{
 	protected void attack( Actor defender ){
 
 		Random r = new Random ();
-		damageDealt = (int) Math.round ( r.nextDouble ()* attack )+ level;
+		double random_attack = r.nextDouble () * attack;
 
-		if ( damageDealt> attack ){
+		damageDealt = (int) Math.round ( random_attack ) + level;
+
+		if ( damageDealt > attack ){
 
 			damageDealt = attack;
 		}else{
@@ -122,11 +125,11 @@ public abstract class Actor extends Entity{
 			// Nothing to do
 		}
 
-		defender.setHP ( defender.getHP ()- damageDealt );
+		defender.setHP ( defender.getHP () - damageDealt );
 		defender.setDamageTaken ( damageDealt );
 		this.setStance ( false , true , false , false );
 
-		if ( damageDealt> 0 ){
+		if ( damageDealt > 0 ){
 
 			defender.setStance ( false , false , true , false );
 		}else{
@@ -146,12 +149,12 @@ public abstract class Actor extends Entity{
 		myTurn = false;
 		nextActor.setTurn ( true );
 
-		/* 
-		Set turn delay if actor is visible.
-		tickTimer = TURN_DELAY if there are any enemies currently visible
-		to the player
-		If player is ending turn
-		*/
+		/*
+		 * Set turn delay if actor is visible. tickTimer = TURN_DELAY if there
+		 * are any enemies currently visible to the player If player is ending
+		 * turn
+		 */
+
 		if ( this instanceof trl.entity.player.Player ){
 
 			// Verify if there any currently visible enemies
@@ -167,7 +170,8 @@ public abstract class Actor extends Entity{
 				}
 			}
 			// Am I a wizard who just exploded (even though I am alone)
-			if ( this instanceof trl.entity.player.Wizard && getTimers ()[1]== 10 ){
+			boolean isWizzardAndTime = this instanceof trl.entity.player.Wizard && getTimers ()[1] == 10;
+			if ( isWizzardAndTime ){
 
 				Game.tickTimer = Game.TURN_DELAY;
 			}
@@ -179,7 +183,7 @@ public abstract class Actor extends Entity{
 
 				Game.tickTimer = Game.TURN_DELAY;
 			}else{
-				
+
 				// Nothing to do
 			}
 		}
@@ -198,12 +202,14 @@ public abstract class Actor extends Entity{
 	// Moves your character to a place if it is possible
 	public void move( Node node ){
 
-		/* 
-		Remove the actor from current node, set loc to argument node,
-		add self to current loc, remove argument node from path.
-		*/
+		/*
+		 * Remove the actor from current node, set loc to argument node, add
+		 * self to current loc, remove argument node from path.
+		 */
 
-		if ( node.getFeature ().isPassable ()&& !node.hasEnemy () ){
+		boolean isFeaturePassableAndHasNoEnemy = node.getFeature ().isPassable () && !node.hasEnemy ();
+
+		if ( isFeaturePassableAndHasNoEnemy ){
 
 			loc.removeEntity ( this );
 			loc = node;
@@ -211,8 +217,8 @@ public abstract class Actor extends Entity{
 			damageDealt = 0;
 			path.remove ( node );
 		}else{
-			
-			//nothing to do
+
+			// nothing to do
 		}
 	}
 
@@ -227,19 +233,19 @@ public abstract class Actor extends Entity{
 	// Plays some sounds dependending on the action
 	protected void playSound(){
 
-		if ( attacked&& damageDealt> 0 ){
+		if ( attacked && damageDealt > 0 ){
 
 			soundManager.playSound ( "strike" );
 		}else{
-			
+
 			// Nothing to do
 		}
 
-		if ( attacked&& damageDealt== 0 ){
+		if ( attacked && damageDealt == 0 ){
 
 			soundManager.playSound ( "miss" );
 		}else{
-			
+
 			// Nothing to do
 		}
 	}
@@ -247,15 +253,14 @@ public abstract class Actor extends Entity{
 	// Actions of the character
 	public boolean getActed(){
 
-		if ( moved|| attacked|| acted ){
+		if ( moved || attacked || acted ){
 
 			return true;
 		}else{
-			
+
 			return false;
 		}
 
-		
 	}
 
 	public int[] getTimers(){
@@ -265,9 +270,9 @@ public abstract class Actor extends Entity{
 
 	public void decrementTimers(){
 
-		for ( int i = 0 ; i< timers.length ; i++ ){
+		for ( int i = 0 ; i < timers.length ; i++ ){
 
-			if ( timers[i]> 0 ){
+			if ( timers[i] > 0 ){
 
 				timers[i]--;
 			}
@@ -318,7 +323,7 @@ public abstract class Actor extends Entity{
 		}
 
 		// If room only has one connection, only option is that room
-		if ( current.getConnectedTo ().size ()== 1 ){
+		if ( current.getConnectedTo ().size () == 1 ){
 
 			nextRoom = current.getConnectedTo ().get ( 0 );
 		}else{
@@ -335,49 +340,50 @@ public abstract class Actor extends Entity{
 	public void render( Graphics g ){
 
 		if ( this.inDisplayedNodes () ){
-			
+
 			// Row and column in displayed nodes
 			int wx = map.getDisplayedX ( loc );
 			int wy = map.getDisplayedY ( loc );
 
 			// Pixel x and y in game window
-			int px = wx* Game.SCALED_TILE_SIZE;
-			int py = Game.W_HEIGHT- ( wy* Game.SCALED_TILE_SIZE )
-					- Game.SCALED_TILE_SIZE;
-	
+			int px = wx * Game.SCALED_TILE_SIZE;
+			int py = Game.W_HEIGHT - ( wy * Game.SCALED_TILE_SIZE ) - Game.SCALED_TILE_SIZE;
+
 			// Height and width in pixels
 			int width = Game.SCALED_TILE_SIZE;
 			int height = Game.SCALED_TILE_SIZE;
 
-			if ( hp> 0 ){
+			if ( hp > 0 ){
 
 				if ( this.getVisibleToPlayer () ){
 
 					g.drawImage ( image , px , py , width , height , null );
 					renderHealthBar ( g , px , py , width , height );
-				}
-				else{
-					
+				}else{
+
 					// Nothing to do
 				}
 				// Draw damage indicator
-				if ( myTurn&& Game.tickTimer> 0&& getDamageTaken ()> 0 ){
+				
+				boolean myTurnAndTimerDamageGreaterZero = myTurn && Game.tickTimer > 0 && getDamageTaken () > 0;
+				
+				if ( myTurnAndTimerDamageGreaterZero ){
 
-					g.drawImage ( Game.getImageManager ().bang , px , py ,
-							width , height , null );
+					g.drawImage ( Game.getImageManager ().bang , px , py , width , height , null );
 					g.setColor ( Color.red );
-					g.drawString ( Integer.toString ( getDamageTaken () ) , px
-							+ width/ 2 , py+ height/ 2 );
+					g.drawString ( Integer.toString ( getDamageTaken () ) , px + width / 2 , py + height / 2 );
 				}else{
-					
+
 					// Nothing to do
 				}
-
-				if ( this instanceof trl.entity.player.Wizard&& timers[1]== 10 ){
+				
+				boolean wizzardAndTimersTen = this instanceof trl.entity.player.Wizard && timers[1] == 10;
+				
+				if ( wizzardAndTimersTen ){
 
 					renderExplosion ( g , px , py );
 				}else{
-					
+
 					// Nothing to do
 				}
 
@@ -387,48 +393,44 @@ public abstract class Actor extends Entity{
 					if ( ( (Enemy) this ).getTargeted () ){
 						g.setColor ( Color.YELLOW );
 						g.drawRect ( px , py , width , height );
-						g.drawRect ( px+ 1 , py+ 1 , width- 2 , height- 2 );
+						g.drawRect ( px + 1 , py + 1 , width - 2 , height - 2 );
 					}else{
-						
+
 						// Nothing to do
 					}
-					
+
 				}else{
-					
+
 					// Nothing to do
 				}
 
 				// If attacking
 				if ( stance[1] ){
 
-					g.drawImage ( Game.getImageManager ().swords , px , py ,
-							width , height , null );
+					g.drawImage ( Game.getImageManager ().swords , px , py , width , height , null );
 				}else{
-					
+
 					// Nothing to do
 				}
 
 				// If shooting
 				if ( stance[3] ){
 
-					g.drawImage ( Game.getImageManager ().arrows , px , py ,
-							width , height , null );
+					g.drawImage ( Game.getImageManager ().arrows , px , py , width , height , null );
 				}else{
-					
+
 					// Nothing to do
 				}
-				
+
 			}else{
 
-				g.drawImage ( Game.getImageManager ().corpse , px , py , width ,
-						height , null );
+				g.drawImage ( Game.getImageManager ().corpse , px , py , width , height , null );
 			}
 		}
 	}
 
 	// Changes the color of health
-	public void renderHealthBar( Graphics g , int x , int y , int width ,
-			int height ){
+	public void renderHealthBar( Graphics g , int x , int y , int width , int height ){
 
 		// Draw bar background
 		Color trBlack = new Color ( 0 , 0 , 0 , 64 );
@@ -438,60 +440,58 @@ public abstract class Actor extends Entity{
 		Color trRed = new Color ( 255 , 0 , 0 , 196 );
 
 		g.setColor ( trGray );
-		g.fillRect ( x+ 1 , y+ (int) ( width* .75 )- 1 , width- 2 , 6 );
-		
+		g.fillRect ( x + 1 , y + (int) ( width * .75 ) - 1 , width - 2 , 6 );
+
 		// Draw border
 		g.setColor ( trBlack );
-		g.drawRect ( x+ 1 , y+ (int) ( height* .75 )- 1 , width- 2 , 6 );
+		g.drawRect ( x + 1 , y + (int) ( height * .75 ) - 1 , width - 2 , 6 );
 
 		// Calculate width of bar
-		double percentHealth = (double) hp/ (double) maxHP;
-		if ( percentHealth> .75 ){
+		double percentHealth = (double) hp / (double) maxHP;
+		if ( percentHealth > .75 ){
 
 			g.setColor ( trGreen );
 		}else{
-			
+
 			// Nothing to do
 		}
-		
-		if ( percentHealth> .25&& percentHealth<= .75 ){
+
+		if ( percentHealth > .25 && percentHealth <= .75 ){
 
 			g.setColor ( trYellow );
 		}else{
-			
+
 			// Nothing to do
 		}
-		
-		if ( percentHealth<= .25 ){
+
+		if ( percentHealth <= .25 ){
 
 			g.setColor ( trRed );
 		}else{
-			
+
 			// Nothing to do
 		}
-		
+
 		// Draw bar
-		g.fillRect ( x+ 2 , y+ (int) ( height* .75 ) ,
-				(int) ( percentHealth* ( width- 4 ) ) , 4 );
+		g.fillRect ( x + 2 , y + (int) ( height * .75 ) , (int) ( percentHealth * ( width - 4 ) ) , 4 );
 	}
 
 	// Renderizes the explosions
 	public void renderExplosion( Graphics g , int x , int y ){
 
 		// Blast radius
-		int r = 0; 
-		if ( this instanceof trl.entity.player.Wizard&& this.timers[1]> 0
-				&& Game.tickTimer> 0 ){
-			
+		int r = 0;
+		if ( this instanceof trl.entity.player.Wizard && this.timers[1] > 0 && Game.tickTimer > 0 ){
+
 			// Determine blast radius at time = timers[1]
 
-			if ( Game.tickTimer== Game.TURN_DELAY ){
+			if ( Game.tickTimer == Game.TURN_DELAY ){
 
 				r = 0;
-			}else if ( Game.tickTimer>= Game.TURN_DELAY/ 2 ){
+			}else if ( Game.tickTimer >= Game.TURN_DELAY / 2 ){
 
 				r = 1;
-			}else if ( Game.tickTimer> 0 ){
+			}else if ( Game.tickTimer > 0 ){
 
 				r = 2;
 			}else{
@@ -502,16 +502,17 @@ public abstract class Actor extends Entity{
 			List<Node> blastArea = map.getAoENodes ( loc , r );
 
 			for ( Node node : blastArea ){
-				g.drawImage (Game.getImageManager ().fire ,map.getDisplayedX ( node )* Game.SCALED_TILE_SIZE ,
-						( Game.W_HEIGHT- Game.SCALED_TILE_SIZE )
-								- ( map.getDisplayedY ( node )* Game.SCALED_TILE_SIZE ) ,
-						Game.SCALED_TILE_SIZE , Game.SCALED_TILE_SIZE , null );
+				
+				int gameSizeY = ( Game.W_HEIGHT - Game.SCALED_TILE_SIZE ) - ( map.getDisplayedY ( node ) * Game.SCALED_TILE_SIZE );
+				
+				int gameSizeX = map.getDisplayedX ( node ) * Game.SCALED_TILE_SIZE;
+				
+				g.drawImage ( Game.getImageManager ().fire , gameSizeX , gameSizeY , Game.SCALED_TILE_SIZE , Game.SCALED_TILE_SIZE , null );
 			}
 		}
 	}
 
-	public void setStance( boolean normal , boolean attacking , boolean hit ,
-			boolean shooting ){
+	public void setStance( boolean normal , boolean attacking , boolean hit , boolean shooting ){
 
 		stance[0] = normal;
 		stance[1] = attacking;
@@ -523,34 +524,38 @@ public abstract class Actor extends Entity{
 	public void closeDoor( Node node ){
 
 		// If node has open door and no entities occupying node.
-		if ( node.getFeature () instanceof trl.map.feature.DoorOpen
-				&& ( node.getEntities ()== null|| node.getEntities ().size ()== 0 ) ){
+		boolean getFeature = node.getFeature () instanceof trl.map.feature.DoorOpen;
+		boolean getEntitiesNullAndSizeZero = node.getEntities () == null || node.getEntities ().size () == 0 ;
+		
+		if ( getFeature && getEntitiesNullAndSizeZero ){
 
 			node.makeClosedDoor ();
 		}else{
-			
+
 			// Nothing to do
 		}
-		
+
 		if ( this instanceof trl.entity.player.Player ){
 
 			GameplayState.getPlayer ().close = false;
 			path.remove ( node );
 		}else{
-			
+
 			// Nothing to do
 		}
-		
+
 	}
 
 	// Opens the door
 	public void openDoor( Node node ){
-
-		if ( node.getFeature () instanceof trl.map.feature.DoorClosed ){
+		
+		boolean featureAndDoorClosed = node.getFeature () instanceof trl.map.feature.DoorClosed;
+			
+		if ( featureAndDoorClosed ){
 
 			node.makeOpenDoor ();
 		}else{
-			
+
 			// Nothing to do
 		}
 		
@@ -558,10 +563,10 @@ public abstract class Actor extends Entity{
 
 			path.remove ( node );
 		}else{
-			
+
 			// Nothing to do
 		}
-		
+
 	}
 
 	public abstract void tick();
