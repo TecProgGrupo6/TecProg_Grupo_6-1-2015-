2cb7738337de78ec73b8b1b0d8e9a9da63eee46c
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-16 11:16:39 -0300

    Logging System to frog gargoyle class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Gargoyle.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Gargoyle.java
@@ -1,5 +1,9 @@
 package trl.entity.enemy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import trl.entity.actor.Actor;
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +14,15 @@ public class Gargoyle extends Enemy{
 		super( map );
 		init();
 	}
+	
+	// Log system from Gargoyle Class
+	private final static Logger LOGGER = Logger.getLogger( Gargoyle.class.getName() );
 
 	// Initiliaze a gargoyle
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Gargoyle intialized");
 
 		maxHP = 30;
 		attack = 5;
