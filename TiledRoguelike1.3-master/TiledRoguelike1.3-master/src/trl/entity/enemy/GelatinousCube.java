5eec35e8b2acbefcd0b3b85c1d2d6314ee7c12c7
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-16 11:17:03 -0300

    Logging System to frog gelatinuous cube class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/GelatinousCube.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/GelatinousCube.java
@@ -1,5 +1,9 @@
 package trl.entity.enemy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import trl.entity.actor.Actor;
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +14,15 @@ public class GelatinousCube extends Enemy{
 		super( map );
 		init();
 	}
+	
+	// Log system from GelatinousCube Class
+	private final static Logger LOGGER = Logger.getLogger( GelatinousCube.class.getName() );
 
 	// Iniitliaze Geletinous Cube
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Gelatinous Cube intialized");
 
 		maxHP = 14;
 		attack = 5;
