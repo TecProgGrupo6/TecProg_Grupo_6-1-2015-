6e55c9e0026cb17b57f750e2b9c61085fb58a71f
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-16 11:15:34 -0300

    Logging System to bat class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Bat.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Bat.java
@@ -1,5 +1,10 @@
 package trl.entity.enemy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import trl.entity.actor.Actor;
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +15,15 @@ public class Bat extends Enemy{
 		super( map );
 		init();
 	}
+	
+	// Log system from Bat Class
+	private final static Logger LOGGER = Logger.getLogger( Bat.class.getName() );
 
 	// Initiliaze Bat
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Bat intialized");
 
 		maxHP = 6;
 		attack = 5;
