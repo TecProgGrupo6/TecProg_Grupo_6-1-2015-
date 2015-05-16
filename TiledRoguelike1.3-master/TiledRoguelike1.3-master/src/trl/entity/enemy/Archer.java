b2d82e75beae7193501768d186a24c18b10945d3
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-16 11:15:16 -0300

    Logging System to archer class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Archer.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Archer.java
@@ -1,7 +1,11 @@
 package trl.entity.enemy;
 
+import trl.entity.actor.Actor;
 import trl.main.Game;
 import trl.map.Map;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Archer extends Enemy{
 
@@ -10,9 +14,14 @@ public class Archer extends Enemy{
 		super( map );
 		init();
 	}
+	
+	private final static Logger LOGGER = Logger.getLogger( Archer.class.getName() );
 
 	// Initiliaze archer
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Archer");
 
 		maxHP = 5;
 		attack = 5;
