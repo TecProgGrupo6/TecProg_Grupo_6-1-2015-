114dc5613d25763dafbe4fae10ff045f59d6d90e
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-16 11:21:34 -0300

    Logging System to wolf class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Wolf.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Wolf.java
@@ -1,5 +1,8 @@
 package trl.entity.enemy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +13,15 @@ public class Wolf extends Enemy{
 		super( map );
 		init();
 	}
+	
+	// Log system from Wolf Class
+	private final static Logger LOGGER = Logger.getLogger( Wolf.class.getName() );
 
 	// Initiliaze Wolf
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Wolf intialized");
 
 		maxHP = 20;
 		attack = 5;
