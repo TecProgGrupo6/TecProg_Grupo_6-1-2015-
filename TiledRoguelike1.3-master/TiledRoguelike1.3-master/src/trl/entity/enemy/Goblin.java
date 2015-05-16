fc207ebd41cf80a41d799dbc0347189584fc5ed1
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-16 11:17:43 -0300

    Logging System to frog gelatinuous goblin class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Goblin.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Goblin.java
@@ -1,5 +1,9 @@
 package trl.entity.enemy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import trl.entity.actor.Actor;
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +14,15 @@ public class Goblin extends Enemy{
 		super( map );
 		init();
 	}
+	
+	// Log system from Goblin Class
+	private final static Logger LOGGER = Logger.getLogger( Goblin.class.getName() );
 
 	// Initiliaze Goblin
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Goblin intialized");
 
 		maxHP = 16;
 		attack = 5;
