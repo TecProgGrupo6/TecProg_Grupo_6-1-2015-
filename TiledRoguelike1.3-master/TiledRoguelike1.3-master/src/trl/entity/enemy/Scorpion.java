9fab316ad6a01bc7afd50d7fc3fb37b3278a7f60
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-16 11:20:21 -0300

    Logging System to scorpion class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Scorpion.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Scorpion.java
@@ -1,5 +1,8 @@
 package trl.entity.enemy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +13,15 @@ public class Scorpion extends Enemy{
 		super( map );
 		init();
 	}
+	
+	// Log system from Scorpion Class
+	private final static Logger LOGGER = Logger.getLogger( Scorpion.class.getName() );
 
 	// Initiliaze Scorpion
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Scorpion intialized");
 
 		maxHP = 12;
 		attack = 5;
