8f20001161f152a92a1f1160ed92c81068487ca2
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-17 10:55:02 -0300

    Logging System to Lock class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Lock.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Lock.java
@@ -1,13 +1,23 @@
 package trl.entity.item;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import trl.main.Game;
 import trl.map.Map;
 
 public class Lock extends Item{
+	
+	// Log system from Lock Class
+	private final static Logger LOGGER = Logger.getLogger( Lock.class.getName() );
 
 	public Lock ( Map map ){
 
 		super( map );
 		this.image = Game.getImageManager().lock;
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Lock intialized");
+		
 	}
 }
