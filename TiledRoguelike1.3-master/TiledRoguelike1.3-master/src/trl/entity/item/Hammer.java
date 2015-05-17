ed0cd3a9cba0600a5fafe82c9886d4111e5d162b
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-17 10:54:24 -0300

    Logging System to Hummer class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Hammer.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Hammer.java
@@ -1,5 +1,8 @@
 package trl.entity.item;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +13,15 @@ public class Hammer extends Item{
 		super( map );
 		init();
 	}
+	
+	// Log system from Hammer Class
+	private final static Logger LOGGER = Logger.getLogger( Hammer.class.getName() );
 
 	// Initiliaze Hammer
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Hammer intialized");
 
 		this.image = Game.getImageManager().hammer;
 		this.loc = map.placeEntity( this , map.getRandomNodeInRoom() );
