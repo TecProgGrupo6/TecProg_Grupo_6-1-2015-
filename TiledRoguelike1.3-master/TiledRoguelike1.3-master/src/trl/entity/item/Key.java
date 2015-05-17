31b1871443bcabc2c5acccbdc8f329ba0cc32c9e
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-17 10:54:46 -0300

    Logging System to Key class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Key.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Key.java
@@ -1,5 +1,9 @@
 package trl.entity.item;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import trl.entity.enemy.Demon;
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +14,15 @@ public class Key extends Item{
 		super( map );
 		init();
 	}
+	
+	// Log system from Key Class
+	private final static Logger LOGGER = Logger.getLogger( Key.class.getName() );
 
 	// Initiliaze Key
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Key intialized");
 
 		this.image = Game.getImageManager().key;
 		this.loc = map.placeEntity( this , map.getRandomNodeInRoom() );
