27c0de43cab178974dc4ea1bb455cda14ef26cd2
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-17 10:55:24 -0300

    Logging System to Potion class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Potion.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Potion.java
@@ -1,5 +1,8 @@
 package trl.entity.item;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +13,15 @@ public class Potion extends Item{
 		super( map );
 		init();
 	}
+	
+	// Log system from Potion Class
+	private final static Logger LOGGER = Logger.getLogger( Potion.class.getName() );
 
 	// Initializes Potion
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Potion intialized");
 
 		this.image = Game.getImageManager().potion;
 		this.loc = map.placeEntity( this , map.getRandomNodeInRoom() );
