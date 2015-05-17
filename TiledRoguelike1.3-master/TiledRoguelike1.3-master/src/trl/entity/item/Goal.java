79c92b13ab996eba5107ca9a84656b8621a8b092
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-17 10:54:07 -0300

    Logging System to Goal class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Goal.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/item/Goal.java
@@ -1,5 +1,8 @@
 package trl.entity.item;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import trl.main.Game;
 import trl.map.Map;
 
@@ -10,9 +13,15 @@ public class Goal extends Item{
 		super( map );
 		init();
 	}
+	
+	// Log system from Goal Class
+	private final static Logger LOGGER = Logger.getLogger( Goal.class.getName() );
 
 	// Initiliaze Goal
 	public void init (){
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Goal intialized");
 
 		this.image = Game.getImageManager().goal;
 		this.loc = map.placeEntity( this , map.getRandomNodeInRoom() );
