f851d311d30a64d1647f5f5db0ac01e1066dc785
MaxOliveira <maxwell_oliveira2@hotmail.com>
2015-05-18 19:11:15 -0300

    Logging system Center class

--- Library/src/Center.java
+++ Library/src/Center.java
@@ -1,12 +1,17 @@
 //import the packages for using the classes in them into this class
 
 import java.awt.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 /**
  * A PUBLIC CLASS FOR CENTER.JAVA
  */
 public class Center{
 
+	// Log system from AddBooks Class
+	private final static Logger LOGGER = Logger.getLogger( AddBooks.class.getName() );
+		
 	JLibrary l; // For using the class in JLibrary.java
 
 	public Center ( JLibrary l ){
@@ -20,6 +25,9 @@ public class Center{
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		l.setLocation( ( screenSize.width - l.getWidth() ) / 2 , ( screenSize.height - l.getHeight() ) / 2 );
+		
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Window centered");
 
 	}
 }
