fb9ba5e6a680f06755b90adbbf95c3fc0ceb1a80
ThiagosLima <thiagolima.unb@gmail.com>
2015-05-18 20:14:37 -0300

    Logging system class StatusBar

--- Library/src/StatusBar.java
+++ Library/src/StatusBar.java
@@ -2,13 +2,20 @@
 
 import javax.swing.*;
 import javax.swing.border.SoftBevelBorder;
+
 import java.awt.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 
 /**
  * A public class
  */
 
 public class StatusBar extends JPanel{
+	
+	// Log system from Toolbar class
+	private final static Logger LOGGER = Logger.getLogger( StatusBar.class.getName() );
 
 	/***************************************************************************
 	 *** declaration of the private variables used in the program ***
@@ -19,6 +26,9 @@ public class StatusBar extends JPanel{
 	// Constructor of StatusBar
 	public StatusBar (){
 
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("StatusBar created.");
+		
 		statusBar.setFont( new Font( "Tahoma" , Font.BOLD , 9 ) );
 		this.add( statusBar );
 		this.setBorder( new SoftBevelBorder( javax.swing.border.BevelBorder.LOWERED ) );
