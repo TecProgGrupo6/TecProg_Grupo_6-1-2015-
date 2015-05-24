5c5e1257a9587a3f49fc3da37b74f54041319f52
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 16:46:31 -0300

    Logging system of Feature class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Feature.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Feature.java
@@ -1,10 +1,18 @@
 package trl.map.feature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Feature{
 
 	protected boolean passable;
+	
+	private final static Logger LOGGER = Logger.getLogger( Feature.class.getName() );
 
 	public boolean isPassable (){
+		
+		LOGGER.setLevel( Level.CONFIG );
+		LOGGER.config("Returned Feature");
 
 		return passable;
 	}
