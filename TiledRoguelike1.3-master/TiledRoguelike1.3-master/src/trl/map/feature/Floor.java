548086ad72c4669c93b070d0c8d86c23136f2e02
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 16:47:13 -0300

    Logging system of Floor class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Floor.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Floor.java
@@ -1,8 +1,16 @@
 package trl.map.feature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Floor extends Feature{
+	
+	private final static Logger LOGGER = Logger.getLogger( Floor.class.getName() );
 
 	public Floor (){
+		
+		LOGGER.setLevel( Level.CONFIG );
+		LOGGER.config("Returned Floor");
 
 		passable = true;
 	}
