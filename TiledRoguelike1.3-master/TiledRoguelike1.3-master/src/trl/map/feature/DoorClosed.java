cf3aef6d3ba3f369113b2ccbf8b58b30e5da01bc
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 16:43:52 -0300

    Loggin system of DoorClosed class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/DoorClosed.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/DoorClosed.java
@@ -1,8 +1,14 @@
 package trl.map.feature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DoorClosed extends Feature{
 
+	private final static Logger LOGGER = Logger.getLogger( DoorClosed.class.getName() );
 	public DoorClosed (){
+		LOGGER.setLevel( Level.INFO );
+		LOGGER.info("Retuned clodes door");
 
 		passable = false;
 	}
