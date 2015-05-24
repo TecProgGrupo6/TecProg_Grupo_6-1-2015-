02ad20b92927dba26fddac7ae60504cecfd75f23
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 16:49:25 -0300

    Logging system of Void class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Void.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Void.java
@@ -1,8 +1,17 @@
 package trl.map.feature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Void extends Feature{
+	
+	private final static Logger LOGGER = Logger.getLogger( Void.class.getName() );
+
 
 	public Void (){
+		
+		LOGGER.setLevel( Level.CONFIG );
+		LOGGER.config("Returned Void");
 
 		passable = false;
 	}
