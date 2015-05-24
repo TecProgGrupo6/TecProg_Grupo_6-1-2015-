5827b09fde68fadb368867ac19a13419e3157385
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 16:48:37 -0300

    Logging system of StairDown class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/StairDown.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/StairDown.java
@@ -1,8 +1,18 @@
 package trl.map.feature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class StairDown extends Feature{
+	
+	private final static Logger LOGGER = Logger.getLogger( StairDown.class.getName() );
+
 
 	public StairDown (){
+		
+		LOGGER.setLevel( Level.CONFIG );
+		LOGGER.config("Returned StairDown");
+
 
 		passable = true;
 	}
