c9efa4ef4c573f18cd8beced622b07956ee10f7e
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 16:50:06 -0300

    Logging sysem of Wall class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Wall.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Wall.java
@@ -1,8 +1,17 @@
 package trl.map.feature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Wall extends Feature{
+	
+	private final static Logger LOGGER = Logger.getLogger( Wall.class.getName() );
+
 
 	public Wall (){
+		
+		LOGGER.setLevel( Level.CONFIG );
+		LOGGER.config("Returned Wall");
 
 		passable = false;
 	}
