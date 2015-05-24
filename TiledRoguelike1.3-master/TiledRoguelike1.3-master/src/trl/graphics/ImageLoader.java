1b4a29f6c76afd1b4b5da36b383b4067bfaeff5a
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 15:19:10 -0300

    Logging system to ImageLoader class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/graphics/ImageLoader.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/graphics/ImageLoader.java
@@ -2,14 +2,21 @@ package trl.graphics;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
+import trl.gamestate.GameStateManager;
+
 public class ImageLoader{
 
+	private final static Logger LOGGER = Logger.getLogger( ImageLoader.class.getName() );
 	public BufferedImage load ( String path ){
 
 		try{
+			LOGGER.setLevel( Level.CONFIG );
+			LOGGER.config("Buffering image completed");
 			return ImageIO.read( getClass().getResource( path ) );
 		}catch ( IOException e ){
 			e.printStackTrace();
