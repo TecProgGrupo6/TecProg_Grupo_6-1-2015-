e1517ec4be5997031022c0f67092ce6e412ef993
Pedro Sales <pedrosal3s@gmail.com>
2015-05-24 16:47:58 -0300

    Logging system of Goal class

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Goal.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/map/feature/Goal.java
@@ -1,8 +1,16 @@
 package trl.map.feature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Goal extends Feature{
+	
+	private final static Logger LOGGER = Logger.getLogger( Goal.class.getName() );
 
 	public Goal (){
+		
+		LOGGER.setLevel( Level.CONFIG );
+		LOGGER.config("Returned Goal");
 
 		passable = true;
 	}
