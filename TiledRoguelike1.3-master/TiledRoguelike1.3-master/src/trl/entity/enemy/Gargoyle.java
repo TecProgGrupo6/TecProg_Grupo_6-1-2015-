481b43b1b4bd96f647922c302ea3eca8d199ef18
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-25 11:47:47 -0300

    Removing warnings!

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Gargoyle.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/Gargoyle.java
@@ -3,7 +3,6 @@ package trl.entity.enemy;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import trl.entity.actor.Actor;
 import trl.main.Game;
 import trl.map.Map;
 
@@ -19,16 +18,17 @@ public class Gargoyle extends Enemy{
 	private final static Logger LOGGER = Logger.getLogger( Gargoyle.class.getName() );
 
 	// Initiliaze a gargoyle
+	@Override
 	public void init (){
 		
 		LOGGER.setLevel( Level.INFO );
 		LOGGER.info("Gargoyle intialized");
 
-		maxHP = 30;
-		attack = 5;
-		image = Game.getImageManager().gargoyle;
-		hp = maxHP;
-		xpReward = 15;
-		level = 15;
+		this.maxHP = 30;
+		this.attack = 5;
+		this.image = Game.getImageManager().gargoyle;
+		this.hp = this.maxHP;
+		this.xpReward = 15;
+		this.level = 15;
 	}
 }
