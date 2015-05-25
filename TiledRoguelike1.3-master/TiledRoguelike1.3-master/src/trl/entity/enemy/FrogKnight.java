4790789499ac8bb97877d1353b5e798ec0da0db8
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-25 11:47:32 -0300

    Removing warnings!

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/FrogKnight.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/entity/enemy/FrogKnight.java
@@ -3,7 +3,6 @@ package trl.entity.enemy;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import trl.entity.actor.Actor;
 import trl.main.Game;
 import trl.map.Map;
 
@@ -19,16 +18,17 @@ public class FrogKnight extends Enemy{
 	private final static Logger LOGGER = Logger.getLogger( FrogKnight.class.getName() );
 
 	// Initialize a frog knight
+	@Override
 	public void init (){
 		
 		LOGGER.setLevel( Level.INFO );
 		LOGGER.info("Frog Knight intialized");
 
-		maxHP = 10;
-		attack = 8;
-		image = Game.getImageManager().frogKnight;
-		hp = maxHP;
-		level = 1;
-		xpReward = 1;
+		this.maxHP = 10;
+		this.attack = 8;
+		this.image = Game.getImageManager().frogKnight;
+		this.hp = this.maxHP;
+		this.level = 1;
+		this.xpReward = 1;
 	}
 }
