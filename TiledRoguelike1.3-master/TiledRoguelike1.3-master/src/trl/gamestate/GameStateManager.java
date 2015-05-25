0342966a1161cfc80b70dd56fe3ce8dec854595c
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-25 11:59:34 -0300

    Removing warnings!

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/gamestate/GameStateManager.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/gamestate/GameStateManager.java
@@ -22,48 +22,49 @@ public class GameStateManager{
 
 	public void init (){
 
-		gameStates = new ArrayList<GameState>();
-		gameStates.add( 0 , new MenuState() ); // Add menu state in the first position of list.
-		currentGameState = gameStates.get( 0 ); // Get the first game state of list.
+		this.gameStates = new ArrayList<GameState>();
+		this.gameStates.add( 0 , new MenuState() ); // Add menu state in the first position of list.
+		this.currentGameState = this.gameStates.get( 0 ); // Get the first game state of list.
 		
 		LOGGER.setLevel( Level.INFO );
-		LOGGER.info( "Initializing GameStateManager. Current game state = " + currentGameState.toString() );
+		LOGGER.info( "Initializing GameStateManager. Current game state = " + this.currentGameState.toString() );
 	}
 
 	public void tick (){
 
-		currentGameState.tick();
+		this.currentGameState.tick();
 	}
 
 	public void render ( Graphics g ){
 
-		currentGameState.render( g );
+		this.currentGameState.render( g );
 	}
 
 	public void setGameState ( int indexState ){
 
-		currentGameState = gameStates.get( indexState );
+		this.currentGameState = this.gameStates.get( indexState );
 	}
 
 	public int getGameState (){
 
-		return gameStates.indexOf( currentGameState );
+		return this.gameStates.indexOf( this.currentGameState );
 	}
 
 	public GameState getGameState ( int index ){
 
-		return gameStates.get( index );
+		return this.gameStates.get( index );
 	}
 
 	public void addGameState ( int index , GameState gameState ){
 
-		gameStates.add( index , gameState );
+		this.gameStates.add( index , gameState );
 	}
 
 	public void removeGameState ( int indexGameState ){
 		
 		int i = 0;
-		for ( Iterator<GameState> iteratorGameState = gameStates.iterator() ; iteratorGameState.hasNext() ; ){
+		for ( Iterator<GameState> iteratorGameState = this.gameStates.iterator() ; iteratorGameState.hasNext() ; ){
+			@SuppressWarnings ( "unused" )
 			GameState nextGameState = iteratorGameState.next();
 			if ( i == indexGameState ){
 				iteratorGameState.remove();
