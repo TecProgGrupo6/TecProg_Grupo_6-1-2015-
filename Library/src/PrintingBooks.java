0849770af068ae34cb5fd04d477b8215e9033f72
ThiagosLima <thiagolima.unb@gmail.com>
2015-05-25 19:47:01 -0300

    Fixing warnings of PrintingBooks class

--- Library/src/PrintingBooks.java
+++ Library/src/PrintingBooks.java
@@ -14,6 +14,11 @@ import java.util.logging.Logger;
 
 public class PrintingBooks extends JInternalFrame implements Printable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	// Log system from PrintingBooks class
 	private final static Logger LOGGER = Logger.getLogger( PrintingBooks.class.getName() );
 
@@ -37,7 +42,7 @@ public class PrintingBooks extends JInternalFrame implements Printable{
 	private JTextArea textArea = new JTextArea();
 
 	// For creating the vector to use it in the print
-	private Vector lines;
+	private Vector<String> lines;
 
 	// Tab size
 	public static final int TAB_SIZE = 5;
@@ -54,10 +59,10 @@ public class PrintingBooks extends JInternalFrame implements Printable{
 		Container cp = getContentPane();
 
 		// For setting the font
-		textArea.setFont( new Font( "Tahoma" , Font.PLAIN , 9 ) );
+		this.textArea.setFont( new Font( "Tahoma" , Font.PLAIN , 9 ) );
 
 		// For adding the textarea to the container
-		cp.add( textArea );
+		cp.add( this.textArea );
 
 		try{
 
@@ -81,24 +86,24 @@ public class PrintingBooks extends JInternalFrame implements Printable{
 
 		try{
 
-			connection = DriverManager.getConnection( URL );
-			statement = connection.createStatement();
-			resultset = statement.executeQuery( query );
-			textArea.append( "=============== Books Information ===============\n\n" );
+			this.connection = DriverManager.getConnection( this.URL );
+			this.statement = this.connection.createStatement();
+			this.resultset = this.statement.executeQuery( query );
+			this.textArea.append( "=============== Books Information ===============\n\n" );
 
-			while ( resultset.next() ){
+			while ( this.resultset.next() ){
 
-				textArea.append( "Subject: " + resultset.getString( "Subject" ) + "\n" + "Title: " + resultset.getString( "Title" ) + "\n"
-						+ "Author(s): " + resultset.getString( "Author" ) + "\n" + "Copyright: " + resultset.getString( "Copyright" )
-						+ "\n" + "Edition: " + resultset.getString( "Edition" ) + "\n" + "ISBN: " + resultset.getString( "ISBN" ) + "\n"
-						+ "Library: " + resultset.getString( "Library" ) + "\n\n" );
+				this.textArea.append( "Subject: " + this.resultset.getString( "Subject" ) + "\n" + "Title: " + this.resultset.getString( "Title" ) + "\n"
+						+ "Author(s): " + this.resultset.getString( "Author" ) + "\n" + "Copyright: " + this.resultset.getString( "Copyright" )
+						+ "\n" + "Edition: " + this.resultset.getString( "Edition" ) + "\n" + "ISBN: " + this.resultset.getString( "ISBN" ) + "\n"
+						+ "Library: " + this.resultset.getString( "Library" ) + "\n\n" );
 
 			}
 
-			textArea.append( "=============== Books Information ===============" );
-			resultset.close();
-			statement.close();
-			connection.close();
+			this.textArea.append( "=============== Books Information ===============" );
+			this.resultset.close();
+			this.statement.close();
+			this.connection.close();
 
 		}catch ( SQLException SQLe ){
 			System.out.println( SQLe.toString() );
@@ -111,6 +116,7 @@ public class PrintingBooks extends JInternalFrame implements Printable{
 	}
 
 	// Printing books
+	@Override
 	public int print ( Graphics pg , PageFormat pageFormat , int pageIndex ) throws PrinterException{
 
 		pg.translate( (int) pageFormat.getImageableX() , (int) pageFormat.getImageableY() );
@@ -119,29 +125,29 @@ public class PrintingBooks extends JInternalFrame implements Printable{
 		int hPage = (int) pageFormat.getImageableHeight();
 		pg.setClip( 0 , 0 , wPage , hPage );
 
-		pg.setColor( textArea.getBackground() );
+		pg.setColor( this.textArea.getBackground() );
 		pg.fillRect( 0 , 0 , wPage , hPage );
-		pg.setColor( textArea.getForeground() );
+		pg.setColor( this.textArea.getForeground() );
 
-		Font font = textArea.getFont();
+		Font font = this.textArea.getFont();
 		pg.setFont( font );
 		FontMetrics fm = pg.getFontMetrics();
 		int hLine = fm.getHeight();
 
-		if ( lines == null ){
+		if ( this.lines == null ){
 
-			lines = getLines( fm , wPage );
+			this.lines = getLines( fm , wPage );
 
 		}else{
 			// No action
 		}
 
-		int numLines = lines.size();
+		int numLines = this.lines.size();
 		int linesPerPage = Math.max( hPage / hLine , 1 );
 		int numPages = (int) Math.ceil( (double) numLines / (double) linesPerPage );
 
 		if ( pageIndex >= numPages ){
-			lines = null;
+			this.lines = null;
 			return NO_SUCH_PAGE;
 		}else{
 			// No action
@@ -151,9 +157,9 @@ public class PrintingBooks extends JInternalFrame implements Printable{
 		int y = fm.getAscent();
 		int lineIndex = linesPerPage * pageIndex;
 
-		while ( lineIndex < lines.size() && y < hPage ){
+		while ( lineIndex < this.lines.size() && y < hPage ){
 
-			String str = (String) lines.get( lineIndex );
+			String str = this.lines.get( lineIndex );
 			pg.drawString( str , x , y );
 			y += hLine;
 			lineIndex++;
@@ -162,11 +168,11 @@ public class PrintingBooks extends JInternalFrame implements Printable{
 		return PAGE_EXISTS;
 	}
 
-	protected Vector getLines ( FontMetrics fm , int wPage ){
+	protected Vector<String> getLines ( FontMetrics fm , int wPage ){
 
-		Vector v = new Vector();
+		Vector<String> v = new Vector<>();
 
-		String text = textArea.getText();
+		String text = this.textArea.getText();
 		String prevToken = "";
 		StringTokenizer st = new StringTokenizer( text , "\n\r" , true );
 
