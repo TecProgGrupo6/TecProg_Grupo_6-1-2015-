a1a072628cc30b0844e495e3ea2baf26d0726bba
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-25 12:04:35 -0300

    Removing warnings!

--- TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/gamestate/WinGameState.java
+++ TiledRoguelike1.3-master/TiledRoguelike1.3-master/src/trl/gamestate/WinGameState.java
@@ -23,63 +23,83 @@ public class WinGameState extends GameState{
 		init();
 	}
 
+	@Override
 	public void init (){
 
-		choices = new String[] { "Yes" , "No" };
-		choice = 0;
-		enemiesDefeated = GameplayState.getPlayer().getEnemiesDefeated();
-		turns = Game.turnCounter;
+		this.choices = new String[] { "Yes" , "No" };
+		this.choice = 0;
+		this.enemiesDefeated = GameplayState.getPlayer().getEnemiesDefeated();
+		this.turns = Game.turnCounter;
+		
 		LOGGER.setLevel( Level.INFO );
 		LOGGER.info("Initializing choice array after win.");
 		
 	}
 
+	@Override
 	public void tick (){
 
 		if ( up ){
-			if ( choice == 0 ){
-				choice = 1;
+			
+			if ( this.choice == 0 ){
+				
+				this.choice = 1;
 			}else{
-				choice = 0;
+				
+				this.choice = 0;
 			}
+			
 			up = false;
 		}else{
+			
 			// Nothing
 		}
 		if ( down ){
-			if ( choice == 1 ){
-				choice = 0;
+			
+			if ( this.choice == 1 ){
+				
+				this.choice = 0;
 			}else{
-				choice = 1;
+				
+				this.choice = 1;
 			}
+			
 			down = false;
 		}else{
+			
 			// Nothing
 		}
 		if ( enter ){
-			if ( choice == 0 ){
+			
+			if ( this.choice == 0 ){
+				
 				Game.getGameStateManager().removeGameState( 0 );
 				Game.getGameStateManager().addGameState( 0 , new MenuState() );
 				Game.getGameStateManager().setGameState( 0 );
 				LOGGER.setLevel( Level.INFO );
 				LOGGER.info("Option 'YES' choiced");
 			}else{
+				
 				// Nothing
 			}
-			if ( choice == 1 ){
+			if ( this.choice == 1 ){
+				
 				LOGGER.setLevel( Level.INFO );
 				LOGGER.info("Option 'NO' choiced");
 				Game.running = false;
 			}else{
+				
 				// Nothing
 			}
 			enter = false;
 		}else{
+			
 			// Nothing
 		}
 
 	}
 
+	@Override
 	public void render ( Graphics g ){
 
 		// int turnsPlayed = Game.turnCounter;
@@ -91,7 +111,7 @@ public class WinGameState extends GameState{
 		g.fillRect( originX , originY , width , height );
 		int linePosition = originY;
 		linePosition += 16;
-		String exitMessage = "You won the game in " + turns + "turns after killing " + enemiesDefeated + " enemies";
+		String exitMessage = "You won the game in " + this.turns + "turns after killing " + this.enemiesDefeated + " enemies";
 		g.setColor( Color.WHITE );
 		// g.drawString(exitMessage, (Game.W_WIDTH -
 		// g.getFontMetrics().stringWidth(exitMessage)) / 2, 0);
@@ -101,13 +121,17 @@ public class WinGameState extends GameState{
 		linePosition += 16;
 		LOGGER.setLevel( Level.CONFIG );
 		LOGGER.config("Rendering graph game");
-		for ( int i = 0 ; i < choices.length ; i++ ){
-			if ( i == choice ){
+		for ( int i = 0 ; i < this.choices.length ; i++ ){
+			
+			if ( i == this.choice ){
+				
 				g.setColor( Color.YELLOW );
 			}else{
+				
 				g.setColor( Color.WHITE );
 			}
-			g.drawString( choices[i] , ( Game.W_WIDTH - g.getFontMetrics().stringWidth( choices[i] ) ) / 2 , linePosition );
+			
+			g.drawString( this.choices[i] , ( Game.W_WIDTH - g.getFontMetrics().stringWidth( this.choices[i] ) ) / 2 , linePosition );
 			linePosition += 16;
 		}
 	}
